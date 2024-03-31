8a8105684ebc754c76fbca8d2166798d108da35d
devship000 <165475600+devship000@users.noreply.github.com>
2024-03-31 06:22:22 -0700

    update frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,69 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
+import Web3 from 'web3';
+import Navbar from './Navbar';
+import SocialNetwork from '../abis/SocialNetwork.json';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      socialNetwork: null,
+      postCount: 0,
+      posts: []
+    }
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    // Network ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = SocialNetwork.networks[networkId]
+    if(networkData) {
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
+      this.setState({ socialNetwork })
+      const postCount = await socialNetwork.methods.postCount().call()
+      this.setState({ postCount })
+      // Load Posts
+      for (var i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({
+          posts: [...this.state.posts, post]
+        })
+      }
+    } else {
+      window.alert('SocialNetwork contract not deployed to detected network.')
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -27,7 +75,7 @@ class App extends Component {
                 >
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
+                <h1>BLOCKCHAIN Starter Kit</h1>
                 <p>
                   Edit <code>src/components/App.js</code> and save to reload.
                 </p>
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Identicon from 'identicon.js';
+
+export default function Navbar(props) {
+    return (
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          BLOCKCHAIN
+        </a>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small className="text-secondary">
+              <small id="account">{props.account}</small>
+            </small>
+            {
+                props.account
+                ? <img className="ml-2" width={30} height={30} src={`data:image/png;base64, ${new Identicon(props.account, 30).toString()}`} />
+                : <span></span>
+            }
+          </li>
+        </ul>
+      </nav>
+    )
+}
